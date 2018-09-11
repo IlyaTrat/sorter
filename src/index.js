2d0049b0f04d94e4963313abce58cea150018778
Трацевский Илья <deeo-sama@hotmail.com>
2018-09-11 20:48:49 +0300

    Done sorting

--- src/index.js
+++ src/index.js
@@ -1,30 +1,59 @@
 class Sorter {
   constructor() {
-    // your implementation
+    this.info = [];
+    this.compFunct = (a, b) => a - b;
   }
 
   add(element) {
-    // your implementation
+    this.info.push(element);
   }
 
   at(index) {
-    // your implementation
+    return this.info[index];
   }
 
   get length() {
-    // your implementation
+    return this.info.length;
   }
 
   toArray() {
-    // your implementation
+    return this.info;
   }
 
+/*   sort(indices) {
+    indices.sort((a, b) => {
+      return a - b;
+    });
+    for(let i = 0; i < indices.length; i++) {
+      for(let j = i; j < indices.length; j++) {
+        let a, b;
+        a = this.info[indices[i]];
+        b = this.info[indices[j]];
+        if(a > b) {
+          this.info.splice(indices[i], 1, b);
+          this.info.splice(indices[j], 1, a);
+        }
+      }
+    }
+  } */
+
   sort(indices) {
-    // your implementation
+    indices.sort((a, b) => a - b);
+    let tempArr = [];
+
+    indices.forEach(element => {
+      tempArr.push(this.info[element]);
+    });
+
+    tempArr.sort(this.compFunct);
+
+    for(let i = 0; i < tempArr.length; i++) {
+      this.info[indices[i]] = tempArr[i];
+    }
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    this.compFunct = compareFunction;
   }
 }
 
